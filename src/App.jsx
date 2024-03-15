207d3304c8a2e9c1bd970b1207077307eb0e5a8f
boniaminshawon <shawonboni29@gmail.com>
2024-03-15 22:01:14 +0600

    initialy set up the project by tailwind and daisyuI

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="container mx-auto border-4 border-black min-h-screen px-20">
+      <Navbar></Navbar>
+
+    </div>
+     
+    </>
+  )
+}
+
+export default App
